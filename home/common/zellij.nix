995eec7234a46aeb3dd7e9d2b32955506f33ee33
yhn <yhn@yhn.fyi>
2025-03-04 14:39:10 +0900

    zellij fish integration off

--- home/common/zellij.nix
+++ home/common/zellij.nix
@@ -1,6 +1,8 @@
 {
   programs.zellij = {
     enable = true;
+# Otherwise it creates a session everytime you open shell which is stupid
+    enableFishIntegration = false; 
     settings = {
       # Make zellij UI more compact
       pane_frames = false;
